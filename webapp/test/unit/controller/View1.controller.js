7789390bca2b4b4e6bfc867beed13cd1f7e77253
D064809 <tom.bendrath@sap.com>
2021-05-06 13:24:12 +0200

    renaming app module & added karma

--- webapp/test/unit/controller/View1.controller.js
+++ webapp/test/unit/controller/View1.controller.js
@@ -1,16 +1,28 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"sapui5demoapp-xx/controller/View1.controller"
+    "sapui5demoappxx/controller/View1.controller"
 ], function (Controller) {
-	"use strict";
+    "use strict";
 
-	QUnit.module("View1 Controller");
+    QUnit.module("View1 Controller");
 
-	QUnit.test("I should test the View1 controller", function (assert) {
-		var oAppController = new Controller();
-		oAppController.onInit();
-		assert.ok(oAppController);
-	});
+    QUnit.test("I should test the View1 controller", function (assert) {
+        var oAppController = new Controller();
+        oAppController.onInit();
+        assert.ok(oAppController);
+    });
 
+    QUnit.test("Test UnitTestExampleCorrect Function", function (assert) {
+        var oAppController = new Controller();
+        var compareValue = oAppController.UnitTestExampleCorrect(3, 3);
+        assert.strictEqual(compareValue, 6, "3 + 3 should give 6 and gives: " + compareValue);
+    });
+
+    // This test will fail if the expected value is 6. Please change to 5 or remove this test
+    QUnit.test("Test UnitTestExampleFailure Function", function (assert) {
+        var oAppController = new Controller();
+        var compareValue = oAppController.UnitTestExampleFailure(3, 3);
+        assert.strictEqual(compareValue, 6, "3 + 3 should give 6 and gives: " + compareValue);
+    });
 });
